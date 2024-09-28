0ba8796491bb839ef9dfe7c4885841a220d39d87
AnhhTuann <tuannguyen25101999@gmail.com>
2024-09-28 16:07:55 +0700

    completed project

--- scripts/app.js
+++ scripts/app.js
@@ -9,3 +9,35 @@ window.addEventListener("click", function (e) {
     menu.classList.remove(activeClass);
   }
 });
+
+window.addEventListener("load", function () {
+  const sliderItems = document.querySelectorAll(".testimonial-item");
+  const sliderItemWidth = sliderItems[0].clientWidth;
+  console.log("sliderItemWidth", sliderItemWidth);
+  const slider = document.querySelector(".testimonial-list");
+  const arrowLeft = document.querySelector(".arrow-left");
+  const arrowRight = document.querySelector(".arrow-right");
+  let positionX = 0;
+  let index = 0;
+  arrowRight.addEventListener("click", function () {
+    arrowLeft.classList.remove("disabled");
+    if (index >= sliderItems.length - 1) {
+      this.classList.add("disabled");
+      return;
+    }
+    positionX += sliderItemWidth;
+    index++;
+    slider.setAttribute("style", `transform:translateX(-${positionX}px);`);
+  });
+  arrowLeft.addEventListener("click", function () {
+    arrowRight.classList.remove("disabled");
+
+    if (index <= 0) {
+      this.classList.add("disabled");
+      return;
+    }
+    positionX -= sliderItemWidth;
+    index--;
+    slider.setAttribute("style", `transform:translateX(-${positionX}px);`);
+  });
+});
